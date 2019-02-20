5f83974f662bf088cdc12173b420b5a18552d356
benjun_xu <894012405@qq.com>
2019-02-20 17:27:57 +0800

    提出routes路由代码文件

--- src/main.js
+++ src/main.js
@@ -1,47 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-import Home from './component/Home'
-import Menu from './component/Menu'
-import Admin from './component/Admin'
-import About from './component/about/About'
-import Login from './component/Login'
-import Register from './component/Register'
-
-//二级路由
-import Contact from './component/about/Contact'
-import Delivery from './component/about/Delivery'
-import History from './component/about/History'
-import OrderingGuide from './component/about/OrderingGuide'
-
-//三级路由
-import Phone from './component/about/contact/Phone'
-import Personname from './component/about/contact/Personname'
+import { routes } from './routes'
 
 Vue.use(VueRouter);
-
-const routes =[
-	{path:'/',name:'homeLink',component:Home},
-	{path:'/menu',name:'menuLink',component:Menu},
-	{path:'/Admin',name:'adminLink',component:Admin},
-	{path:'/About',name:'aboutLink', redirect:'/about/contact',component:About, children:[
-		{path:'/about/contact', name:'contactLink', redirect:'/personname', component:Contact, children:[
-			{path:'/phone',name:'phoneNumber',component:Phone},
-			{path:'/personname',name:'personName',component:Personname},
-		]},
-		{path:'/history', name:'historyLink', component:History},
-		{path:'/delivery', name:'deliveryLink', component:Delivery},
-		{path:'/orderingGuide', name:'orderingGuideLink', component:OrderingGuide},
-	]},
-	{path:'/Login',name:'loginLink',component:Login},
-	{path:'/Register',name:'registerLink',component:Register},
-	{path:'*',redirect:'/'},
-];
 
 const router = new VueRouter({
 	routes,
 	mode:'history'
 })
+
+//全局守卫
+/*router.beforeEach((to, from, next) => {
+	//alert('还没有登录，请先登录');
+	//next()
+
+	console.log(to);
+
+	//判断store.gettes.isLogin == false
+	if(to.path == '/login' || to.path == '/register'){
+		next()
+	}else{
+		alert('还没有登录，请先登录');
+		next('/login')
+	}
+})*/
+
+//后置钩子
+/*router.afterEach((to,from)=>{
+	alert('alert each')
+})*/
 
 new Vue({
   el: '#app',
---
+++ src/routes.js
@@ -0,0 +1,48 @@
+import Home from './component/Home'
+import Menu from './component/Menu'
+import Admin from './component/Admin'
+import About from './component/about/About'
+import Login from './component/Login'
+import Register from './component/Register'
+
+//二级路由
+import Contact from './component/about/Contact'
+import Delivery from './component/about/Delivery'
+import History from './component/about/History'
+import OrderingGuide from './component/about/OrderingGuide'
+
+//三级路由
+import Phone from './component/about/contact/Phone'
+import Personname from './component/about/contact/Personname'
+
+export const routes =[
+	{path:'/',name:'homeLink',component:Home},
+	{path:'/menu',name:'menuLink',component:Menu},
+	{path:'/admin',name:'adminLink',component:Admin, 
+	  //beforeEnter: (to, from, next)=>{
+		//路由独享守卫
+		//alert('非登录状态，不能访问此页');
+		//next(false);
+
+		//判断store.gettes.isLogin == false
+		/*if(to.path == '/login' || to.path == '/register'){
+			next()
+		}else{
+			alert('还没有登录，请先登录');
+			next('/login')
+		}*/
+		//}
+	},
+	{path:'/about',name:'aboutLink', redirect:'/about/contact',component:About, children:[
+		{path:'/about/contact', name:'contactLink', redirect:'/personname', component:Contact, children:[
+			{path:'/phone',name:'phoneNumber',component:Phone},
+			{path:'/personname',name:'personName',component:Personname},
+		]},
+		{path:'/history', name:'historyLink', component:History},
+		{path:'/delivery', name:'deliveryLink', component:Delivery},
+		{path:'/orderingGuide', name:'orderingGuideLink', component:OrderingGuide},
+	]},
+	{path:'/login',name:'loginLink',component:Login},
+	{path:'/register',name:'registerLink',component:Register},
+	{path:'*',redirect:'/'},
+];
