ec491b064e48a952fa515236febdedd74216cb1e
benjun_xu <894012405@qq.com>
2019-02-21 11:23:51 +0800

    设计menu菜单

--- src/component/about/Delivery.vue
+++ src/component/about/Delivery.vue
@@ -2,7 +2,7 @@
 	<div class="card text-dark bg-light mb-3">
 		<div class="card-header">快递信息</div>
 		<div class="card-body">
-			<h4 class="card-title">联系我们</h4>
+			<h4 class="card-title">快递信息</h4>
 			<p class="card-text">894012405@qq.com</p>
 		</div>
 	</div>		
--- src/main.js
+++ src/main.js
@@ -2,12 +2,26 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes'
+import axios from 'axios'
 
 Vue.use(VueRouter);
 
+axios.defaults.baseURL = "https://wd3451744778iotmac.wilddogio.com/"
+
 const router = new VueRouter({
 	routes,
-	mode:'history'
+	mode:'history',
+	scrollBehavior(to,from,savePosition){
+		//return {x:0,y:100}
+
+		//return {selector: '.btn'}
+
+		if(savePosition){
+			return savePosition
+		}else{
+			return {x:0,y:0}
+		}
+	}
 })
 
 //全局守卫
